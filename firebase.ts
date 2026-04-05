19b8ee90cff3a4c8fb834748303719cd87956158
Will <william.xiao24@imperial.ac.uk>
2026-04-06 00:49:47 +0100

    ???????

--- firebase.ts
+++ firebase.ts
@@ -11,14 +11,14 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBuk5LRCSLaIg_alDTTbB0yA1d7x4IoP2I",
-  authDomain: "fellwanderers-85f0d.firebaseapp.com",
-  projectId: "fellwanderers-85f0d",
-  storageBucket: "fellwanderers-85f0d.appspot.com",
-  messagingSenderId: "440459668854",
-  appId: "1:440459668854:web:313c631a7081a7e1a31e57",
-  measurementId: "G-NZPXJZ8ZLH"
+  apiKey: "AIzaSyCbfuHvkr7s8u88FTfz9kFMlMQ6cDIB7wQ",
+  authDomain: "icfellwanderers.firebaseapp.com",
+  projectId: "icfellwanderers",
+  storageBucket: "icfellwanderers.firebasestorage.app",
+  messagingSenderId: "954474029334",
+  appId: "1:954474029334:web:0e740663eecbeb70af7037"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
