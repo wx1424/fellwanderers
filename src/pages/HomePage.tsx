d7237128f04714cffef06dfbb928a68365978202
Will <william.xiao24@imperial.ac.uk>
2026-04-06 02:46:51 +0100

    new hike ppics

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -16,7 +16,7 @@ const joinLink =
 
 function HeroSection() {
   const backgroundImageStyle = {
-    backgroundImage: `url(/hike_pics/LakeDistrict22.jpg)`,
+    backgroundImage: `url(/hike_pics/lake-district-2025.jpg)`,
   };
   return (
     <section
@@ -71,26 +71,26 @@ function StickyDescription() {
     {
       title: "About Us",
       para: "We are Fellwanderers, the hiking society for Imperial College London - your gateway to discovering the breathtaking landscapes and hidden gems of the United Kingdom. We will take you on unforgettable hiking experiences from day hikes, thrilling weekend trips, and captivating holiday tours across the UK. We are also looking for experienced hikers to help out with leading these trips, so get in touch if you are interested.",
-      image: "hike_pics/punchbowl23.jpg",
-      alt: "Devil's Punchbowl 2023"
+      image: "hike_pics/isle-of-skye-2026.jpg",
+      alt: "Isle of Skye 2026"
     },
     {
       title: "Explore Nature, Create Memories",
       para: "But we're more than just a hiking club; we're a community of like-minded individuals who share a passion for the great outdoors. When you join us, you're not just signing up for hikes; you're joining a group of kindred spirits who relish the opportunity to escape the bustling city of London and immerse themselves in the tranquility of nature. Our hikes aren't just about the breathtaking views and challenging trails; they're about the connections you make and the conversations you have along the way.",
-      image: "hike_pics/peaks23.jpg",
-      alt: "Peak District 2023"
+      image: "hike_pics/bergamesque-alps-2025.jpg",
+      alt: "Bergamesque Alpss 2025"
     },
     {
       title: "Hiking and So Much More",
       para: "From striking up great conversations while trekking through beautiful landscapes to unforgettable social events both on campus and in the heart of London, our club offers a vibrant social aspect that complements our outdoor adventures. Whether it's a casual chat during a hike or a lively gathering at one of our socials, you'll find yourself surrounded by welcoming faces and fantastic company.",
-      image: "hike_pics/south_downs23.jpeg",
-      alt: "South Downs 2023"
+      image: "hike_pics/lake-district-2025-2.jpg",
+      alt: "Lake District 2025"
     },
     {
       title: "Join the Adventure",
       para: "If you're seeking a club that combines the thrill of hiking through stunning locations with the joy of forging meaningful connections, you've found your perfect match. Join us, and let's explore the wonders of the UK together, one step at a time. At Fellwanderers, we're not just a club; we're a community of nature enthusiasts, adventurers, and friends. Get ready to leave the city behind, embark on unforgettable journeys, and create memories that will last a lifetime. Welcome to Fellwanderers!",
-      image: "hike_pics/yorkshire22.jpg",
-      alt: "Yorkshire 2022"
+      image: "hike_pics/surrey-three-peaks-2025.jpg",
+      alt: "Surrey Three Peaks 2025"
     },
   ]
   
