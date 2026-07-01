be406614710121c59febcadb99f41c237ae9de6b
Will <william.xiao24@imperial.ac.uk>
2026-07-01 20:42:10 +0100

    pictures

--- firebase.ts
+++ firebase.ts
@@ -14,7 +14,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCbfuHvkr7s8u88FTfz9kFMlMQ6cDIB7wQ",
   authDomain: "icfellwanderers.firebaseapp.com",
   projectId: "icfellwanderers",
-  storageBucket: "icfellwanderers.appspot.com",
+  storageBucket: "icfellwanderers.firebasestorage.app",
   messagingSenderId: "954474029334",
   appId: "1:954474029334:web:0e740663eecbeb70af7037"
 };
